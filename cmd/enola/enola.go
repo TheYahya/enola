70487cd1c745d371621a3a6087e2774cd50dd548
Yahya SayadArbabi <yahya.arbabi@gmail.com>
2022-12-11 19:24:35 +0300

    fix(cli): fix cli structure

--- cmd/enola/enola.go
+++ cmd/enola/enola.go
@@ -1,34 +1,40 @@
 package main
 
 import (
-	"context"
+	"errors"
 	"fmt"
 	"os"
 
-	"github.com/charmbracelet/bubbles/list"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/sherlock-project/enola"
+	"github.com/spf13/cobra"
 )
 
-type responseMsg enola.Result
+var rootCmd = &cobra.Command{
+	Use:   "enola {username}",
+	Short: "A command-line tool to find username on websites",
+	Args: func(_ *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("can't run without argument, give me a username")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		username := args[0]
+		siteFlag := cmd.Flag("site")
+		findAndShowResult(username, siteFlag.Value.String())
+	},
+}
 
-func findAndShowResult(username, site string) {
-	ctx := context.Background()
-	sh, err := enola.New(ctx)
-	if err != nil {
-		panic(err)
-	}
-
-	m := model{
-		list: list.New([]list.Item{}, NewDelegate(), 0, 0),
-		res:  sh.SetSite(site).Check(username),
-	}
-
-	m.list.Title = "Socials"
-	p := tea.NewProgram(&m, tea.WithAltScreen())
-
-	if err := p.Start(); err != nil {
-		fmt.Println("Error running program:", err)
+func main() {
+	if err := Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.Flags().StringP("site", "s", "", "to only search an specific site")
 }
