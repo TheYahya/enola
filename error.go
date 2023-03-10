2dca4102e7040dd4f8085646d611adfeff22693f
Amir Sadegh <amirrossein@gmail.com>
2023-03-10 10:42:37 +0300

    fix(enola): handle unsupported website

--- error.go
+++ error.go
@@ -2,3 +2,4 @@ package enola
 
 const ErrNoDataFileExists = "err no data file exists"
 const ErrDataFileIsNotAValidJson = "err data file is not a valid json"
+const ErrSiteNotFound = "err requested site is not supported"
