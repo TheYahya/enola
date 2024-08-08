b9b9c09b29574ca34e6e6f0dcf0fc70ea4cf237e
Kartikey Semwal <semwal.kartikey999@gmail.com>
2024-08-08 23:29:41 +0530

    fix if the directory does not exists

--- cmd/exporter/utils.go
+++ cmd/exporter/utils.go
@@ -3,10 +3,20 @@ package exporter
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 func OpenOrCreateFile(filename string) (*os.File, error) {
+	dir := filepath.Dir(filename)
+	// 0755 permissions: the owner can read, write, and execute, everyone else can only read and execute.
+	err := os.MkdirAll(dir, 0755)
+
+	if err != nil {
+		fmt.Println("Error creating directory:", err)
+		return nil, err
+	}
+
 	// Open the file with read and write permissions
 	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
